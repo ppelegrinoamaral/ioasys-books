76dde491d1a6d8b7c4b54d4739b40a9e2d58c5a4
ppelegrinoamaral <paulamaralspi@hotmail.com>
2021-08-27 23:20:46 -0300

    created Header component

---
+++ src/components/Header/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './header.css';
+import Logo from '../../assets/Logo.png';
+
+class Header extends Component {
+    render() {
+        return <div className='headerCard'>
+            <img className='headerLogo' src={Logo}></img>
+            <h1 className='headerTitle'>Books</h1>
+        </div>
+    }
+}
+
+export default Header;
--- src/components/LoginForm/loginForm.js
+++ src/components/LoginForm/loginForm.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import "./loginForm.css";
-import EmailForm from '../EmailForm/emailForm.js'
-import PasswordForm from '../PasswordForm/passwordForm.js'
+import EmailForm from '../EmailForm/emailForm.js';
+import PasswordForm from '../PasswordForm/passwordForm.js';
+import Header from '../Header/header.js';
 
 class LoginForm extends Component {
     render() {
         return <div>
+            <Header />
             <EmailForm />
             <PasswordForm />
         </div>
